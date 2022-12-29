89657eb57cfdb2d5ce52569bcdd8f1a05b31157a
aaron-mini <erbnlegend@gmail.com>
2022-12-29 12:57:34 -0600

    Work on Hero and started weather

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,13 +3,17 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero'>
-      <svg className='openTag' viewBox="0 0 14.499959 18" strokeLinecap="round" strokeLinejoin="round" fill='none' stroke='currentColor' strokeWidth='2' >
+      <svg className='openTag' viewBox="0 0 14.499959 18" strokeLinecap="round" strokeLinejoin="round" fill='none' stroke='currentColor' strokeWidth='1' >
         <path
           d="m 4.9999586,1 -4.00000001,3.9999999 4.00000001,4"
         />
       </svg>
-      <div>I am a creative, problem solving front-end developer</div>
-      <svg className='closeTag' viewBox="0 0 14.499959 18" strokeLinecap="round" strokeLinejoin="round" fill='none' stroke='currentColor' strokeWidth='2'>
+      <div className='quote'>
+        <h1 className='text-bg'>Hello!</h1>
+        <div>I am a creative, problem solving front-end developer</div>
+        <div className='author'>- Aaron Erb</div>
+      </div>
+      <svg className='closeTag' viewBox="0 0 14.499959 18" strokeLinecap="round" strokeLinejoin="round" fill='none' stroke='currentColor' strokeWidth='1'>
         <path d="m 9.5,13 4,-4 -4,-4"/>
         <path d="M 6,1 1,17"/>
       </svg>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { Sun } from 'lucide-react'
 
 import { Moon } from 'lucide-react'
 
 const Navbar = () => {
+  function userLocation () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showUserPosition)
+    } else {
+      DOMElements.error.innerHTML = 'You have disabled Location or is otherwise incapacitated'
+    }
+  }
+  userLocation()
+  function showUserPosition (showUserPosition) {
+    const latitude = showUserPosition.coords.latitude
+    const longitude = showUserPosition.coords.longitude
+    const position = { lat: latitude, lon: longitude }
+    console.log(`Latitude: ${latitude} Longitude: ${longitude}`)
+}
   return (
     <nav className='navbar'>
-      {/* Weather */}
-      <ul>
+      <Sun className='weather'/>
+      <ul className='nav'>
         <NavLink to='./#projects'><li>Projects</li> </NavLink>
         <NavLink to='about'><li>About</li> </NavLink>
+        <li className='toggleTheme'><Moon className='moon'/></li>
       </ul>
-      <div className='toggleTheme'><Moon /></div>
     </nav>
   )
 }
