2035a708e0ae84b5bca70d6e92e2e98e2ca04a87
Aaron <erbnlegend@gmail.com>
2023-01-12 16:02:48 -0600

    production mode automation

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,82 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: {
-    bundle: './src/index.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  mode: 'production',
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+module.exports = (env, argv) => {
+  const ENV = argv.mode || 'development'
+  return {
+    entry: {
+      bundle: './src/index.js'
     },
-    modules: ['node_modules'],
-    extensions: ['.js', '.json']
-  },
-  optimization: {
-    minimize: true,
-    runtimeChunk: 'single',
-    removeEmptyChunks: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        use: ['babel-loader']
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    mode: ENV,
+    devtool: ENV === 'production' ? undefined : 'inline-source-map',
+    devServer: {
+      historyApiFallback: true
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
       },
-      {
-        test: /.(p?css|postcss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'imgs/[name][ext]'
+      modules: ['node_modules'],
+      extensions: ['.js', '.json']
+    },
+    optimization: {
+      minimize: ENV === 'production',
+      runtimeChunk: 'single',
+      removeEmptyChunks: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          use: ['babel-loader']
+        },
+        {
+          test: /.(p?css|postcss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: ''
+              }
+            },
+            'css-loader',
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'imgs/[name][ext]'
+          }
+        },
+        {
+          test: /.(html)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: '[name][ext]',
+            outputPath: './'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource'
         }
-      },
-      {
-        test: /.(html)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: '[name][ext]',
-          outputPath: './'
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      }),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: ENV
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.css'
-    })
-  ]
+  }
 }
